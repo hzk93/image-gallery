d81c54fb575c0cd8f0c822843bfd1386a2fa8cb4
Hassan <hassanzk93@gmail.com>
2022-12-19 10:26:40 +0500

    Trying on my own

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import ImageCard from './components/ImageCard';
 
-function App() {
+const App = () => {
+  const children = [];
+  for(let i=0; i<26; i++){
+    children.push(<ImageCard key={`id-${i}`}/>);
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="px-4 py-6 grid grid-cols-4 gap-4">
+      {children}
     </div>
-  );
+  )
 }
 
 export default App;
